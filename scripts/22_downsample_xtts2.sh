c0860f880829f987ea9543ca65e2b9bcaf7db760
jzmzhong <jinzuomuzhong@gmail.com>
2025-01-14 13:48:02 +0000

    feat(corrupt): more corrupted audios

--- scripts/22_downsample_xtts2.sh
+++ scripts/22_downsample_xtts2.sh
@@ -1,2 +1,4 @@
 # python downsample.py --sr 22050 --in-dir "../xtts_24k" --out-dir "../xtts_22.05k"
-python downsample.py --sr 22050 --in-dir "../xtts_24k_corrupt150k" --out-dir "../xtts_22.05k_corrupt150k"
+python downsample.py --sr 22050 --in-dir "../xtts_24k_corrupt50k" --out-dir "../xtts_22.05k_corrupt50k"
+python downsample.py --sr 22050 --in-dir "../xtts_24k_corrupt100k" --out-dir "../xtts_22.05k_corrupt100k"
+# python downsample.py --sr 22050 --in-dir "../xtts_24k_corrupt150k" --out-dir "../xtts_22.05k_corrupt150k"
--- scripts/23_loudness_addsil_xtts2.sh
+++ scripts/23_loudness_addsil_xtts2.sh
@@ -1,2 +1,4 @@
 # python loudness_addsil.py --volume 0.4 --sil 100 --in-dir ../xtts_22.05k --out-dir ../xtts_22.05k_vol0.4_100ms
-python loudness_addsil.py --volume 0.4 --sil 100 --in-dir ../xtts_22.05k_corrupt150k --out-dir ../xtts_22.05k_corrupt150k_vol0.4_100ms
+python loudness_addsil.py --volume 0.4 --sil 100 --in-dir ../xtts_22.05k_corrupt50k --out-dir ../xtts_22.05k_corrupt50k_vol0.4_100ms
+python loudness_addsil.py --volume 0.4 --sil 100 --in-dir ../xtts_22.05k_corrupt100k --out-dir ../xtts_22.05k_corrupt100k_vol0.4_100ms
+# python loudness_addsil.py --volume 0.4 --sil 100 --in-dir ../xtts_22.05k_corrupt150k --out-dir ../xtts_22.05k_corrupt150k_vol0.4_100ms
