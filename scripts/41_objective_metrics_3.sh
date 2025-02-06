1aadb8cb70bc8abada3d70ebc457a246b7b1f9b0
Jinzuomu Zhong <s2526235@uhtred.inf.ed.ac.uk>
2025-02-06 19:00:24 +0000

    feat(metric): add objective metrics inference using Amphion

---
+++ scripts/41_objective_metrics_3.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+source /home/s2526235/miniconda3/bin/activate amphion_py39torch20cuda117
+
+DATA_DIR=/home/s2526235/accent-evaluation
+CODE_DIR=/home/s2526235/Amphion
+
+declare -A SYSTEMS
+SYSTEMS["copysyn"]="copysyn_hifiganv1"
+SYSTEMS["xtts"]="xtts_22.05k_vol0.4_100ms"
+SYSTEMS["xtts10k"]="xtts_22.05k_corrupt10k_vol0.4_100ms"
+SYSTEMS["xtts20k"]="xtts_22.05k_corrupt20k_vol0.4_100ms"
+SYSTEMS["xtts30k"]="xtts_22.05k_corrupt30k_vol0.4_100ms"
+SYSTEMS["xtts40k"]="xtts_22.05k_corrupt40k_vol0.4_100ms"
+SYSTEMS["xtts50k"]="xtts_22.05k_corrupt50k_vol0.4_100ms"
+SYSTEMS["xtts60k"]="xtts_22.05k_corrupt60k_vol0.4_100ms"
+SYSTEMS["xtts70k"]="xtts_22.05k_corrupt70k_vol0.4_100ms"
+SYSTEMS["xtts80k"]="xtts_22.05k_corrupt80k_vol0.4_100ms"
+SYSTEMS["xtts90k"]="xtts_22.05k_corrupt90k_vol0.4_100ms"
+SYSTEMS["xtts100k"]="xtts_22.05k_corrupt100k_vol0.4_100ms"
+SYSTEMS["xtts110k"]="xtts_22.05k_corrupt110k_vol0.4_100ms"
+SYSTEMS["xtts120k"]="xtts_22.05k_corrupt120k_vol0.4_100ms"
+SYSTEMS["xtts130k"]="xtts_22.05k_corrupt130k_vol0.4_100ms"
+SYSTEMS["xtts140k"]="xtts_22.05k_corrupt140k_vol0.4_100ms"
+SYSTEMS["xtts150k"]="xtts_22.05k_corrupt150k_vol0.4_100ms"
+
+cd ${CODE_DIR}/pretrained
+sh offline_models_setup.sh
+cd ..
+
+for SYS in xtts50k xtts100k xtts150k;
+do
+	for ACC in Edinburgh;
+	do
+		for SPK in p252 p262 p272 p281 p285;
+		do
+			echo ${DATA_DIR}/${SYSTEMS[${SYS}]}/${ACC}/${SPK}
+			sh egs/metrics/run.sh \
+				--reference_folder ${DATA_DIR}/ref_22.05k/${ACC}/${SPK} \
+				--generated_folder ${DATA_DIR}/${SYSTEMS[${SYS}]}/${ACC}/${SPK} \
+				--dump_folder ${DATA_DIR}/objective_analysis \
+				--metrics "fpc f0_periodicity_rmse f0rmse v_uv_f1 cer wer similarity mcd" \
+				--similarity_model wavlm \
+				--similarity_mode pairwith \
+				--intelligibility_mode gt_content \
+				--ltr_path ${DATA_DIR}/scripts/all_transcription.txt \
+				--language english
+
+			wait
+			mv ${DATA_DIR}/objective_analysis/result.json ${DATA_DIR}/objective_analysis/result_${SYS}_${ACC}_${SPK}.json 
+		done
+	done
+done
--- scripts/concat.sh
+++ scripts/concat.sh
@@ -7,13 +7,14 @@ output="combined.txt"
 > "$output"
 
 # Loop through each .txt file in the current directory
-for file in *.txt; do
+for file in `ls ../gt_trans/*/*/*.txt`; do
+    echo $file
     # Ensure we're processing only files
     if [ -f "$file" ]; then
         # Read the line from the file
         line_content=$(<"$file")
         # Append the prefix and line content to the output file
-        echo "$(basename "$file" .txt): $line_content" >> "$output"
+        echo "$(basename "$file" .txt).wav|$line_content" >> "$output"
     fi
 done
 
